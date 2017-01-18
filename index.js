6e0f0b7a55a4d2e01b65718b3558bde698c23c98
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-01-18 18:58:20 +0100

    first pass

--- index.js
+++ index.js
@@ -1,295 +1,448 @@
+var Highlight = require('highlight-syntax')
 var mount = require('choo/mount')
 var html = require('choo/html')
 var log = require('choo-log')
 var css = require('sheetify')
 var choo = require('choo')
 
+css('highlight-syntax-pastel')
 css('tachyons')
 
+var highlight = Highlight([ require('highlight-syntax/js') ])
 var prefix = css`
   :host { background-color: rgb(255, 195, 228) }
 `
 
+function code (str) {
+  return html`
+   <pre class="center lh-copy tl measure-wide-l mt0-ns db bg-dark-gray pa4 mv0 f3 overflow-auto"><code>${
+     toHtml(highlight(str.trim(), { lang: 'js' }))
+   }</code></pre>
+  `
+}
+
 var slides = [
   html`
     <main class="mw7">
+      <h1 class="f-subheadline f-subheadline-ns bold sans-serif mb4">
+        Meditation and mindfulness: a guide to success
+      </h1>
+      <h2 class="f2 mt0 bold">
+        <em>A seminar by Y. Wuyts</em>
+      </h2>
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h1 class="f-subheadline f-subheadline-ns bold sans-serif mb4">
+        just kidding
+      </h1>
+    </main>
+  `,
+  html`
+    <main class="mw7">
       <h1 class="f-subheadline f-headline-ns bold sans-serif mb4">
-        <span>CH</span>
-        <span style="letter-spacing: -0.3em;"> </span>
-        <span>O</span>
-        <span style="letter-spacing: 0.07em;"> </span>
-        <span>O</span>
+        Go! Go! Nanoframeworks!
       </h1>
-      <h2 class="f1 code bold ttu">
-        boarding the tiny framework 🚂🚃🚃🚃
-      </h2>
-      <h2 class="f2 mt0 code bold">
-        [ @yoshuawuyts ]
+      <h2 class="f2 mt0 bold">
+        <em>@yoshuawuyts</em>
       </h2>
     </main>
   `,
   html`
     <main class="mw7">
-      <h2 class="f1 code bold ttu">
-        What are we talking about today?
+      <h1 class="f-subheadline f-headline-ns bold sans-serif mb4">
+        (loud applause)
+      </h1>
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h1 class="f-subheadline f-headline-ns bold sans-serif mb4">
+        HELLO MY NAME IS YOSH
+      </h1>
+      <h2 class="f2 mt0 bold">
+        (hi yosh)
       </h2>
-      <ul class="list f2 lh-copy">
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h2 class="f-5 bold ttu">
+        Today's seminar:
+      </h2>
+      <ul class="list f1 lh-copy">
         <li class="underline">
-          __Why build a framework?________
+          ___Analyzing the patient__
         </li>
         <li class="underline">
-          ___Lessons learned_.
+          _________Anatomy class_.
         </li>
         <li class="underline">
-          What are the new things coming up?_
+          Building a Franken Framework__
         </li>
       </ul>
     </main>
   `,
   html`
     <main class="mw7">
-      <h2 class="f-5 code bold ttu">
-        Building things is fun!
+      <h2 class="f-subheadline bold">
+        So what's a framework anyway?
       </h2>
     </main>
   `,
   html`
     <main class="mw7">
-      <h2 class="f1 ttu">To quote Julia Evans (b0rk)</h3>
-      <ol class="list f2 lh-copy">
+      <p class="f2 bold">
+        Yosh's framework definition:
+      </p>
+      <h2 class="f-subheadline bold">
+        Frameworks are a giant ball of libraries, glue code & opinions
+      </h2>
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h2 class="f1 bold ttu">
+        Modern frameworks contain
+      </h2>
+      <ul class="list f1 lh-copy">
         <li class="underline">
-          1. Ask questions!
+          Render-to-HTML language
         </li>
-        <li class="underline mt3">
-          2. _____Run into problems other people don't know the answer to
+        <li class="underline">
+          Batch rendering
         </li>
-        <li class="underline mt3">
-          3. Just because other people don't know a thing it doesn't mean you
-          can't figure out how to do the thing___________
+        <li class="underline">
+          Data flow management
+        </li>
+        <li class="underline">
+          ________Routing________
+        </li>
+        <li class="underline">
+          Component lifecycle events
         </li>
       </ul>
     </main>
   `,
   html`
     <main class="mw7">
-      <h1 class="f-5 ttu" style="text-align: end">
-        Syntax time!
+      <h1 class="f-5 ttu">
+        Act I: Render-to-HTML language
       </h1>
     </main>
   `,
   html`
     <main class="mw7">
-      <h1 class="f-5 ttu mb0">
-        sup choo
+      <h2 class="f1 ttu">
+        Low Level DOM API
+      </h2>
+${code(`
+var el = document.createElement('section')
+el.setAttribute('class', 'my cool classnames')
+
+var child = document.createElement('div')
+child.textContent = 'hello world'
+
+document.body.appendChild(el)
+`)}
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h2 class="f1 ttu">
+        Template String HTML
+      </h2>
+${code(`
+var html = require('bel')
+var el = html\`
+  <section class="my cool classnames">
+    <div>hello world</div>
+  </section>
+\`
+document.body.appendChild(el)
+`)}
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h1 class="f-5 ttu">
+        Act II: Batch Rendering
       </h1>
-      <pre class="f3 tl mt4 self-start pa4" style="background-color: #fff">
-        ${`
-var html = require('choo/html')
-var choo = require('choo')
-var app = choo()
-app.router(['/', mainView])
-document.body.appendChild(app.start())
-function mainView () {
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h2 class="f1 ttu">
+        Problem: updating DOM elements is expensive
+      </h2>
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h2 class="f1">
+        RequestAnimationFrame (RAF)
+      </h2>
+${code(`
+function rafLoop () {
+  window.RequestAnimationFrame(function () {
+    /* update the DOM right here */
+    rafLoop() // yay recursion
+  })
+}
+rafLoop()
+`)}
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h2 class="f1 ttu">
+        Nanoraf
+      </h2>
+${code(`
+var nanoraf = require('nanoraf')
+
+var render = nanoraf(function () {
+  /* update the DOM right here */
+})
+
+render()
+`)}
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h2 class="f1 ttu">
+        Updating the DOM
+      </h2>
+${code(`
+function toggleClass (elementState) {
+  var parent = document.querySelector('#js-my-element')
+  var child = parent.childNodes[0]
+
+  if (elementState.isOff)
+    child.setAttribute('class', 'on')
+  } else {
+    child.setAttribute('class', 'off')
+  }
+}
+`)}
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h2 class="f1 ttu">
+        Updating the DOM <br> functional rendering
+      </h2>
+${code(`
+function toggleClass (state) {
+  var _class = state.isOff ? 'on' : 'off'
   return html\`
-    <h1>hello tokyo</h1>
+    <section>
+      <div class=\${_class}</div>
+    </section>
   \`
 }
-        `
-        }
-      </pre>
+`)}
     </main>
   `,
   html`
     <main class="mw7">
-      <h1 class="f-5 ttu">
-        choo origin. .story time
-      </h1>
-    </main>
-  `,
-  html`
-    <main class="mw7">
-      <h2 class="f1 ttu" style="text-align: right">
-        A framework that's...
-      </h1>
-      <ul class="list f2 lh-copy">
-        <li class="underline mt3">
-          ,fast to build
-        </li>
-        <li class="underline mt3">
-          ,,performant
-        </li>
-        <li class="underline mt3">
-          ,,,understandable
-        </li>
-        <li class="underline mt3">
-          ,,,,hackable
-        </li>
-      </ul>
-    </main>
-  `,
-  html`
-    <main class="mw7">
-      <h1 class="f-5 ttu">
-        Building frameworks
-      </h1>
-      <ul class="list f2 lh-copy">
-        <li class="underline mt2">
-          Always room for things to be added__
-        </li>
-        <li class="underline mt2">
-          Large surface area________
-        </li>
-        <li class="underline mt2">
-          Lots of glue code__
-        </li>
-        <li class="underline mt2">
-          ___ Tradeoffs are hard
-        </li>
-      </ul>
-    </main>
-  `,
-  html`
-    <main class="mw7">
-      <h1 class="f-5 ttu" style="text-align: left">
-        frameworks make cool ideas accessible
-      </h1>
-    </main>
-  `,
-  html`
-    <main class="mw7">
-      <h1 class="f-5 ttu" style="text-align: left">
-        What if we could do dom diffing without a virtual DOM?
-      </h1>
-    </main>
-  `,
-  html`
-    <main class="mw7">
-      <h1 class="f-5 ttu" style="text-align: left">
-        Morphdom!
-      </h1>
-    </main>
-  `,
-  html`
-    <main class="mw7">
-      <h1 class="f-5 ttu" style="text-align: left">
-        [ MORPHDOM DEMO ]
-      </h1>
-    </main>
-  `,
-  html`
-    <main class="mw7">
-      <h1 class="f-5 ttu" style="text-align: left">
-        What if we could turn our router into a sitemap?
-      </h1>
-    </main>
-  `,
-  html`
-    <main class="mw7">
-      <h1 class="f-5 ttu mb0">
-        sup router
-      </h1>
-      <pre class="f3 tl mt4 self-start pa4" style="background-color: #fff">
-        ${`
-var html = require('choo/html')
-var choo = require('choo')
-var app = choo()
-app.router([
-  ['/', mainView]
-  ['/foo', fooView]
-  ['/bin', [
-    ['/:bar', binBarView]
-  ]]
-])
-document.body.appendChild(app.start())
-}
-        `
-        }
-      </pre>
-    </main>
-  `,
-  html`
-    <main class="mw7">
-      <h1 class="f-5 ttu">
-        , so how does this all perform?
-      </h1>
-      <h2 class="f1" style="text-align: right">
-        batched rendering performance
+      <h2 class="f1 ttu">
+        Updating the DOM <br> Diffing views
       </h2>
+${code(`
+var morph = require('nanomorph')
+var html = require('bel')
+
+var tree = html\`<div>hello people</div>\`
+tree = morph(html\`<div>nanananana-na-no</div>\`, tree)
+tree = morph(html\`<div>tiny tin bottle</div>\`, tree)
+`)}
     </main>
   `,
   html`
     <main class="mw7">
-      <h1 class="f1 ttu underline">
-        what are choo's 🆒 ideas
-      </h1>
-    </main>
-  `,
-  html`
-    <main class="mw7">
-      <h1 class="f-5 ttu" style="text-align: right">
-        event-based recursive asynchronous data flow
-      </h1>
-    </main>
-  `,
-  html`
-    <main class="mw7">
-      <h1 class="f-5 ttu" style="text-align: justify">
-        framework-independent elements
-      </h1>
-    </main>
-  `,
-  html`
-    <main class="mw7">
-      <h1 class="f-5 ttu" style="text-align: justify">
-        <input onkeydown=${(e) => e.preventDefault()} placeholder="and DOM diffing">
-      </h1>
-    </main>
-  `,
-  html`
-    <main class="mw7">
-      <h1 class="f-5 ttu" style="text-align: justify">
-        <input onkeydown=${(e) => e.preventDefault()}>
-      </h1>
-      <h1 class="f-5 ttu" style="text-align: justify">
-        yay for dom diffing!
+      <h1 class="f-5 ttu">
+        Act III: Data Flow
       </h1>
     </main>
   `,
   html`
     <main class="mw7">
       <h1 class="f-5 ttu">
-        where to after [_v4.0_]
+        *crickets*
       </h1>
-      <ul class="list f2 lh-copy">
-        <li class="underline mt2">
-          ++iterate BANKAI streaming asset compiler
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h1 class="f-5 ttu">
+        Act IV: Routing
+      </h1>
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h2 class="f1 bold ttu">
+        Requirements:
+      </h2>
+      <ul class="list f1 lh-copy">
+        <li class="underline">
+          Render views____
         </li>
-        <li class="underline mt2">
-          _____build a server framework (merry!)
+        <li class="underline">
+          ____History API
         </li>
-        <li class="underline mt2">
-          build out {base,form}-elements______--
+        <li class="underline">
+          URL destructuring
         </li>
-        <li class="underline mt2">
-          ______improve perf
+        <li class="underline">
+          Param_____ support
         </li>
-        <li class="underline mt2">
-          improve models____
+        <li class="underline">
+          Route caching
         </li>
       </ul>
     </main>
   `,
   html`
     <main class="mw7">
-      <h1 class="f-5 ttu">
-        [ show off bankai && sheetify ? ]
-      </h1>
+      <h2 class="f1 ttu">
+        Sheet-router
+      </h2>
+${code(`
+var sheetRouter = require('sheet-router')
+var router = sheetRouter([
+  ['/:username', [
+    ['/orgs', orgView]
+  ]],
+  ['/404', notFoundView]
+])
+router('/hughsk/orgs')
+`)}
     </main>
   `,
   html`
     <main class="mw7">
       <h1 class="f-5 ttu">
-        [ demo time ]
+        Act V: Component lifecycle
+      </h1>
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h2 class="f1 bold ttu">
+        Events make the DOM go round
+      </h2>
+      <ul class="list f2 lh-copy">
+        <li class="underline">
+          Has a user interacted with an element?
+        </li>
+        <li class="underline">
+          Is an element mounted on the DOM?
+        </li>
+        <li class="underline">
+          Is an element unmounted from the DOM?
+        </li>
+        <li class="underline">
+          Does the browser have time to render?
+        </li>
+        <li class="underline">
+          Do we need to re-render or cache?
+        </li>
+        <li class="underline">
+          Is my element currently visible?
+        </li>
+      </ul>
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h2 class="f1 ttu">
+        Handling interaction
+      </h2>
+${code(`
+var html = require('bel')
+
+html\`
+  <button onclick=\${handleClick}>
+  </button>
+\`
+function handleClick (e) {
+  console.log('clicked by', e.target)
+}
+`)}
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h2 class="f1 ttu">
+        Is an element mounted?
+      </h2>
+${code(`
+var onload = require('on-load')
+
+var el = html\`<div>hey</div>\`
+onload(el, loaded, unloaded)
+
+function loaded () {
+  console.log('element loaded')
+}
+
+function unloaded () {
+  console.log("and we're gone")
+}
+`)}
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h2 class="f1 ttu">
+        Is an element in view?
+      </h2>
+${code(`
+var onintersect = require('on-intersect')
+
+var el = html\`<div>hey</div>\`
+onintersect(el, entered, exited)
+
+function entered () {
+  console.log('element in view')
+}
+
+function exited () {
+  console.log("and we're gone")
+}
+`)}
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h2 class="f1 ttu">
+        Does the browser have spare time?
+      </h2>
+${code(`
+var polite = require('polite-element')
+html\`
+  <section>
+    \${polite(renderBasic, renderFancy)}
+  </section>
+\`
+
+function renderBasic () {
+  return html\`<div> waitingggg</div> \`
+}
+function renderFancy () {
+  return html\`<div>annnnd we're here!</div> \`
+}
+`)}
+    </main>
+  `,
+  html`
+    <main class="mw7">
+      <h1 class="f-5 ttu">
+        [ hacking time ]
       </h1>
     </main>
   `,
@@ -303,6 +456,9 @@ document.body.appendChild(app.start())
       </h2>
       <h2 class="f2">
         GitHub / yoshuawuyts
+      </h2>
+      <h2 class="f4 underline">
+        https://github.com/yoshuawuyts/go-go-nanoframeworks-talk
       </h2>
     </main>
   `
@@ -375,3 +531,9 @@ app.router(slides.map((slide, i) => {
 }))
 
 mount('body', app.start())
+
+function toHtml (str) {
+  var el = html`<div></div>`
+  el.innerHTML = str
+  return el.childNodes[0]
+}
